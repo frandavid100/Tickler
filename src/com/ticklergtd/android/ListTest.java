90ea92f3c7e8c17170339d4fda156810ce72794d
Eladio Ruiz <eladioruiz@gmail.com>
2011-05-09 23:22:31 +0200

    new row design and inflation

--- src/com/ticklergtd/android/ListTest.java
+++ src/com/ticklergtd/android/ListTest.java
@@ -1,79 +1,182 @@
-/*
- * Copyright (C) 2007 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.ticklergtd.android;
 
 import java.util.ArrayList;
 
-import android.app.ListActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.ViewAnimator;
+import android.widget.ViewFlipper;
 
 import com.ticklergtd.android.model.ContextTask;
 import com.ticklergtd.android.model.Task;
 
-
-/**
- * A list view example where the 
- * data for the list comes from an array of strings.
- */
-public class ListTest extends ListActivity  implements OnClickListener {
-
-	private ArrayList<String> mStrings;
-	private ArrayList<Task> tsk = new ArrayList<Task>();
+public class ListTest extends Activity implements OnClickListener {
 	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	float downXvalue;
+	private ArrayList<String> mStrings_full;
+	private ArrayList<String> mStrings_smart;
+	private ArrayList<Task> tsk_full = new ArrayList<Task>();
+	private ArrayList<Task> tsk_smart = new ArrayList<Task>();
+	private ViewFlipper listsViewFlipper = null;
+	private ListView lvSmart;
+	private ListView lvFull;
+	private TextView txtView;
+	private View addNewTask;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.task_lists);
+		
+        findViews();
+		initViews();
+		setListeners();
+		
+	}
+	
+	// Some tests with viewFlipper in order to switch between Smart List and Full List
+	// TODO: Remove this code and upgrade to something similar to Launcher. Drag and slide views by using gestures.
+	@Override
+	public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.textView_View_Title:
+            {
+            	if (isFullList()) {
+            		listsViewFlipper.setAnimation(AnimationUtils.loadAnimation(this, R.anim.slide_in_left));
+                    ((ViewAnimator) listsViewFlipper).showPrevious();
+                    
+                    txtView.setText(R.string.smart_list_view_title);
+                }
+            	else if (isSmartList()) {
+            		listsViewFlipper.setAnimation(AnimationUtils.loadAnimation(this, R.anim.slide_in_right));
+                    ((ViewAnimator) listsViewFlipper).showNext();
+                    
+                    txtView.setText(R.string.full_list_view_title);
+                }
 
-        // Recupera una lista de tareas
-        tsk = Task.getTasks(ListTest.this,1);
-        
-        // Y ya en una función local, compongo los strings como se necesite
-        mStrings = getStringsFromTasks(tsk);
-        
-        // Y esa lista de string se pasa al adaptador de la lista para que lo muestre
-        setListAdapter(new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, mStrings));
-        getListView().setTextFilterEnabled(true);
+                break;
+            }
+            case R.id.button_Title_Bar_Add_New_Task:
+            {
+            	callTaskEditorActivity(0);
+            	break;
+            }
+        }
     }
-
-	public void onListItemClick(ListView parent, View v, int position,long id) {
+	
+	public void onListItemClick(View v, int position) {
 		//
-		Task tskNew = new Task(ListTest.this);
-		Task tskAux = tsk.get(position);
-		tskNew.setName(tskAux.getName() + ".");
-		tskNew.setPriority(tskAux.getPriority());
-		tskNew.setNote(tskAux.getNote());
-		tskNew.setAbandoned(tskAux.getAbandoned());
-		tskNew.setCompleted(tskAux.getCompleted());
-		tskNew.setContexts(tskAux.getContexts());
-		tskNew.setCreationDate(tskAux.getCreationDate());
-		tskNew.setDeadline(tskAux.getDeadline());
-		tskNew.setOrder(tskAux.getOrder());
-		tskNew.setRepeat(tskAux.getRepeat());
-		tskNew.setRepeatFrom(tskAux.getRepeatFrom());
-		tskNew.setSimultaneous(tskAux.isSimultaneous());
-		tskNew.setSomeday(tskAux.getSomeday());
-		tskNew.setStartDate(tskAux.getStartDate());
-		tskNew.setRepeatUnits(tskAux.getRepeatUnits());
-		tskNew.save();
+		long lTaskId = 0;
+		
+		switch (v.getId()) {
+			case R.id.list_full:
+				lTaskId = getTaskIdFromList(tsk_full, position);
+				break;
+			case R.id.list_smart:
+				lTaskId = getTaskIdFromList(tsk_smart, position);
+				break;
+		}
+		
+		callTaskEditorActivity(lTaskId);
+	}
+
+
+	/* **************************************
+	 * PRIVATE FUNCTIONS 
+	 * **************************************/
+	
+	private void findViews() {
+		lvSmart = (ListView) findViewById(R.id.list_smart);
+		lvFull 	= (ListView) findViewById(R.id.list_full);
+		listsViewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper_Lists_Container);
+		txtView = (TextView)findViewById(R.id.textView_View_Title);
+		addNewTask = findViewById(R.id.button_Title_Bar_Add_New_Task);
+	}
+	
+	private void initViews() {
+        // Recupera una lista de tareas
+        tsk_full = Task.getTasks(ListTest.this,1);
+        tsk_smart = Task.getTasks(ListTest.this,2);
+
+        // Y ya en una función local, compongo los strings como se necesite
+        mStrings_full = getStringsFromTasks(tsk_full);
+        mStrings_smart = getStringsFromTasks(tsk_smart);
+        
+        lvSmart.setAdapter(new IconicAdapter(mStrings_smart));
+        lvSmart.setTextFilterEnabled(true);
+        
+        lvFull.setAdapter(new IconicAdapter(mStrings_full));
+        lvFull.setTextFilterEnabled(true);
+        
+        txtView.setText(R.string.smart_list_view_title);
+	}
+	
+	private void setListeners() {
+		txtView.setOnClickListener(this);
+		addNewTask.setOnClickListener(this);
+		/*lvSmart.setOnClickListener(this);
+		lvFull.setOnClickListener(this);*/
+		lvSmart.setOnItemClickListener(new ListView.OnItemClickListener() {
+	        @Override
+	        public void onItemClick(AdapterView<?> a, View v, int i, long l) {
+	            try {
+	                // Remembers the selected Index
+	            	onListItemClick(lvSmart,i);
+	            }
+	            catch(Exception e) {
+	                System.out.println("Nay, cannot get the selected index");
+	            }
+	        }
+		});
+
+		lvFull.setOnItemClickListener(new ListView.OnItemClickListener() {
+	        @Override
+	        public void onItemClick(AdapterView<?> a, View v, int i, long l) {
+	            try {
+	                // Remembers the selected Index
+	            	onListItemClick(lvFull,i);
+	            }
+	            catch(Exception e) {
+	                System.out.println("Nay, cannot get the selected index");
+	            }
+	        }
+		});
+
+	}
+	
+	// Calls to each optional settings dialog.
+	private void callTaskEditorActivity(long task_id) {
+		Intent taskEditorIntent = new Intent(this,TaskEditorActivity.class);
+		
+		if (task_id > 0) {
+			taskEditorIntent.putExtra("task_id",task_id);
+		}
+		startActivity(taskEditorIntent);	
+	}
+	
+	private boolean isSmartList () {
+		boolean bRes = false;
+		
+		bRes = (txtView.getText().toString().equals(getResources().getString(R.string.smart_list_view_title)));
+		return bRes;
+	}
+	
+	private boolean isFullList () {
+		boolean bRes = false;
+		
+		bRes = (txtView.getText().toString().equals(getResources().getString(R.string.full_list_view_title)));
+		return bRes;
 	}
 	
     // De una lista de tareas, genera una lista de strings con la información
@@ -100,7 +203,7 @@ public class ListTest extends ListActivity  implements OnClickListener {
     	res = tsk.getName();
     	
     	if (tsk.getStartDate() != null) {
-    		String aux = " - " + Utilities.date2String(tsk.getStartDate(),2) + " - (" + getNameContexts(tsk) + ")";
+    		String aux = " - " + Utilities.date2String(tsk.getStartDate(),2) + "##" + getNameContexts(tsk);
     		res += aux;
     	}
     	
@@ -118,12 +221,79 @@ public class ListTest extends ListActivity  implements OnClickListener {
     		res += ct.getName() + "-";
     	}
     	
+    	res = res.substring(0,res.length() - 1);
+    	
     	return res;
     }
+    
+    private long getTaskIdFromList(ArrayList<Task> tsk, int position) {
+    	Task auxTask;
+    	long lRes = 0;
+    	
+    	auxTask = tsk.get(position);
+    	
+    	if (auxTask != null) {
+    		lRes = auxTask.getId();
+    	}
+    	
+    	return lRes;
+    }
+    
+    class IconicAdapter extends ArrayAdapter<String> {
+    	private ArrayList<String> localArray;
+        
+    	IconicAdapter(ArrayList<String> mStrings) {
+        	super(ListTest.this, R.layout.task_lists_row, R.id.textView_lists_row_task_name, mStrings);
+        	localArray = mStrings;
+        }
+        
+        public View getView(int position, View convertView,
+                            ViewGroup parent) {
+        	
+        	String name		= "";
+        	String contexts	= "";
+        	
+        	View row=super.getView(position, convertView, parent);
+        	ImageView icon=(ImageView)row.findViewById(R.id.imageView_lists_row_edit_task);
+        	TextView txtTaskName 		= (TextView)row.findViewById(R.id.textView_lists_row_task_name);
+        	TextView txtTaskContexts 	= (TextView)row.findViewById(R.id.textView_lists_row_task_contexts);
 
-	@Override
-	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		
-	}
+        	if (localArray.get(position).length()>4) {
+        		icon.setImageResource(R.drawable.delete);
+        	}
+        	else {
+        		icon.setImageResource(R.drawable.exit2);
+        	}
+
+        	name 		= getItemParts(localArray.get(position), 1);
+        	contexts 	= getItemParts(localArray.get(position), 2);
+        	txtTaskName.setText(name);
+        	txtTaskContexts.setText(contexts);
+
+        	return(row);
+        }
+        
+        private String getItemParts(String pOriginal, int i) {
+        	int iPos = 0;
+        	String name = "";
+        	String contexts = "";
+        	
+        	iPos = pOriginal.indexOf("##");
+        	if (iPos > 0) {
+        		name 		= pOriginal.substring(0, iPos - 1);
+        		contexts 	= pOriginal.substring(iPos + 2);
+        	}
+        	else {
+        		name 		= pOriginal;
+        		contexts 	= "";
+        	}
+        	
+        	if (i == 1) {
+        		return name;
+        	}
+        	else {
+        		return contexts;
+        	}
+        }
+    }
 }
